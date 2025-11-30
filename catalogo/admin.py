3ff506680c1653f5c3dc2a90d76a47e975bec2e4
cecyramos <c.e.c.y_92_@hotmail.com>
2025-11-29 23:16:31 -0500

    admin.py y capturas

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Editorial, Autor, Libro, PerfilAutor
 
-# Register your models here.
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'pais', 'anio_fundacion']
+    search_fields = ['nombre', 'pais']
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'nacionalidad', 'fecha_nacimiento']
+    search_fields = ['nombre', 'apellido']
+    list_filter = ['nacionalidad']
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'isbn', 'editorial', 'fecha_publicacion']
+    search_fields = ['titulo', 'isbn']
+    list_filter = ['editorial', 'fecha_publicacion']
+    filter_horizontal = ['autores']  # Widget para ManyToMany
+
+@admin.register(PerfilAutor)
+class PerfilAutorAdmin(admin.ModelAdmin):
+    list_display = ['autor', 'email', 'sitio_web']
+    search_fields = ['autor__nombre', 'autor__apellido']
--- prestamos/admin.py
+++ prestamos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Prestamo
 
-# Register your models here.
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ['libro', 'usuario', 'fecha_prestamo', 'fecha_devolucion_esperada', 'estado']
+    list_filter = ['estado', 'fecha_prestamo']
+    search_fields = ['libro__titulo', 'usuario__username']
+    date_hierarchy = 'fecha_prestamo'
